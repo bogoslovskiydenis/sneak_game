78c4f617377ed65e0142c3fd6c93a4511c1e409e
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2022-09-05 16:03:28 +0300

    pixel border

--- lib/hope_screen.dart
+++ lib/hope_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sneak_game/widgets/pixel_border.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,19 +9,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int rowSize = 10;
+  int totalNumberSqueares = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Column(
         children: [
           //scores
-          Expanded(child: Container(color: Colors.red,)),
+          Expanded(child: Container()),
           //game grid
-          Expanded(child: Container(color: Colors.black,)),
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+              itemCount: totalNumberSqueares,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: rowSize),
+              itemBuilder: (BuildContext context, int index) {
+                return PixelBorder();
+              },
+            ),
+          ),
 
           //play btn
-          Expanded(child: Container(color: Colors.blue,)),
-
+          Expanded(
+            child: Container(),
+          ),
         ],
       ),
     );
