86acc2c905f79e2a71caf397ae734a81c03d8a02
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2022-09-05 18:18:01 +0300

    snake game logik (up , right , down , left)

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,8 @@ import 'package:sneak_game/widgets/foor_pixel.dart';
 import 'package:sneak_game/widgets/pixel_border.dart';
 import 'package:sneak_game/widgets/sneak_pixel.dart';
 
+enum snake_Direction { UP, DOWN, LEFT, RIGHT }
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -13,6 +15,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //rowParametrs
   int rowSize = 10;
   int totalNumberSqueares = 100;
 
@@ -22,16 +25,69 @@ class _HomePageState extends State<HomePage> {
   //food position
   int foodPos = 55;
 
-  //
-  void startGame(){
-    Timer.periodic(const Duration(milliseconds: 200), (timer) {
+  //snake direction
+  var currentDirection = snake_Direction.RIGHT;
+
+  //startGame
+  void startGame() {
+    Timer.periodic(const Duration(milliseconds: 500), (timer) {
       setState(() {
-        //add head
-        sneaPosition.add(sneaPosition.last +1);
-        //remove tail
-        sneaPosition.removeAt(0);
+        moveSnake();
       });
     });
+  }
+
+  void moveSnake() {
+    switch (currentDirection) {
+      case snake_Direction.RIGHT:
+        {
+          //if snake is at the right wall , need readjust
+          if (sneaPosition.last % rowSize == 9) {
+            sneaPosition.add(sneaPosition.last + 1 - rowSize);
+          } else {
+            sneaPosition.add(sneaPosition.last + 1);
+          }
+          //remove tail
+          sneaPosition.removeAt(0);
+        }
+        break;
+      case snake_Direction.LEFT:
+        {
+          //add a head
+          if (sneaPosition.last % rowSize == 0) {
+            sneaPosition.add(sneaPosition.last - 1 + rowSize);
+          } else {
+            sneaPosition.add(sneaPosition.last - 1);
+          }
+          //remove tail
+          sneaPosition.removeAt(0);
+        }
+        break;
+      case snake_Direction.UP:
+        {
+          if(sneaPosition.last <rowSize){
+            sneaPosition.add(sneaPosition.last + rowSize + totalNumberSqueares);
+          } else {
+            sneaPosition.add(sneaPosition.last - rowSize);
+          }
+          //remove tail
+          sneaPosition.removeAt(0);
+        }
+        break;
+      case snake_Direction.DOWN:
+        {
+          //add a head
+          if (sneaPosition.last + rowSize > totalNumberSqueares) {
+            sneaPosition.add(sneaPosition.last + rowSize - totalNumberSqueares);
+          } else {
+            sneaPosition.add(sneaPosition.last + rowSize);
+          }
+          //remove tail
+          sneaPosition.removeAt(0);
+        }
+        break;
+      default:
+    }
   }
 
   @override
@@ -45,31 +101,58 @@ class _HomePageState extends State<HomePage> {
           //game grid
           Expanded(
             flex: 3,
-            child: GridView.builder(
-              itemCount: totalNumberSqueares,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: rowSize),
-              itemBuilder: (BuildContext context, int index) {
-                if (sneaPosition.contains(index)) {
-                  return const SneakPixel();
-                } else if (foodPos == index) {
-                  return const FoodPosition();
-                }
-                else {
-                  return const PixelBorder();
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (details.delta.dy > 0 &&
+                    currentDirection != snake_Direction.UP) {
+                  currentDirection = snake_Direction.DOWN;
+                  print('movie down');
+                } else if (details.delta.dy < 0 &&
+                    currentDirection != snake_Direction.DOWN) {
+                  currentDirection = snake_Direction.UP;
+                  print('move up');
                 }
               },
+              onHorizontalDragUpdate: (details) {
+                if (details.delta.dx > 0 &&
+                    currentDirection != snake_Direction.RIGHT) {
+                  currentDirection = snake_Direction.RIGHT;
+                  print('movie right');
+                } else if (details.delta.dx < 0 &&
+                    currentDirection != snake_Direction.RIGHT) {
+                  currentDirection = snake_Direction.LEFT;
+                  print('movie left');
+                }
+              },
+              child: GridView.builder(
+                itemCount: totalNumberSqueares,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: rowSize),
+                itemBuilder: (BuildContext context, int index) {
+                  if (sneaPosition.contains(index)) {
+                    return const SneakPixel();
+                  } else if (foodPos == index) {
+                    return const FoodPosition();
+                  } else {
+                    return const PixelBorder();
+                  }
+                },
+              ),
             ),
           ),
 
           //play btn
           Expanded(
-            child: Center(child: MaterialButton(
-              color: Colors.blue, onPressed: () {
-              startGame();
-            },
-              child: const Text('PLAY'),),),
+            child: Center(
+              child: MaterialButton(
+                color: Colors.blue,
+                onPressed: () {
+                  startGame();
+                },
+                child: const Text('PLAY'),
+              ),
+            ),
           ),
         ],
       ),
